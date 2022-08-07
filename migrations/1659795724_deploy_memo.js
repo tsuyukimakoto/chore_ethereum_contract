df6a094b0756e44119e1bb351f7936ab965e47cf
makoto tsuyuki <mtsuyuki@gmail.com>
2022-08-07 11:56:11 +0900

    truffleのものを全部消す

--- migrations/1659795724_deploy_memo.js
+++
@@ -1,6 +0,0 @@
-const MemoContract = artifacts.require("Memo.sol");
-
-module.exports = function (_deployer) {
-  // Use deployer to state migration tasks.
-  _deployer.deploy(MemoContract, "Memo");
-};
